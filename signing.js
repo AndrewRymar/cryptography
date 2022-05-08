b631fddb7ec6a5ee2c15fadfe39e3bebb9d60910
Andrew Rymar <Andrew.Rymarr@gmail.com>
2022-05-08 21:18:06 +0300

    Added encrypiton and signing

---
+++ signing.js
@@ -0,0 +1,19 @@
+const { createSign, createVerify } = require('crypto');
+const { publicKey, privateKey } = require('./keypair');
+
+const data = 'this data must be signed';
+
+const signer = createSign('rsa-sha256');
+signer.update(data);
+
+const siguature = signer.sign(privateKey, 'hex');
+
+console.log(siguature);
+
+const verifier = createVerify('rsa-sha256');
+verifier.update(data);
+
+const isVerified = verifier.verify(publicKey, siguature, 'hex');
+
+console.log(isVerified);
+
